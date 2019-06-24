87951dd8003279a4daa351921b4365f4f27022b6
Gerewy <jc.jeremycaudron@gmail.com>
2019-06-24 09:41:39 +0200

    2 files modified

--- src/app/contact-service/outside/outside.component.ts
+++ src/app/contact-service/outside/outside.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { numberservice } from '../numberservice'
+import {numberservice} from 'app/numberservice'
+
 
 @Component({
   selector: 'app-outside',
