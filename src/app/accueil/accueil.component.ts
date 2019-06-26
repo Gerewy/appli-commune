4708a2b6e9f93343d6b9e7dd00b66b41121c9a07
Gerewy <50520155+Gerewy@users.noreply.github.com>
2019-06-26 09:25:19 +0200

    Add files via upload

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 
+import { numberservice } from '../contact-service/numberservice'
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent implements OnInit {
+export class AccueilComponent {
+
+  numberservice = numberservice;
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
